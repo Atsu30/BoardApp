1ea52dc92cf3a30045b8e81d9a7a3018ef610f98
atsmr <f.acchan333@gmail.com>
2019-05-13 00:47:11 +0900

    edit login function

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'hello_world#hello'
 
-  get 'signin' => "users#new"
-  post 'signin' => "users#create"
+  get 'signup' => "users#new"
+  post 'signup' => "users#create"
   get 'login' => "sessions#new"
   post "login" => "sessions#create"
 
